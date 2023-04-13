5c0e5be6174456ab5e593473e3bf77eaf242d73c
Dennis Stanistan <dennisstanistan@gmail.com>
2023-04-13 17:01:29 +0300

    Update UInterpTrackAkAudoEvent

--- Plugins/Wwise/Source/AkAudio/Public/InterpTrackAkAudioEvent.h
+++ Plugins/Wwise/Source/AkAudio/Public/InterpTrackAkAudioEvent.h
@@ -2,10 +2,11 @@
 
 //CROSS-MODULE INCLUDE V2: -ModuleName=Engine -ObjectName=InterpTrackVectorBase -FallbackName=InterpTrackVectorBase
 #include "AkAudioEventTrackKey.h"
+#include "Matinee/InterpTrackVectorBase.h"
 #include "InterpTrackAkAudioEvent.generated.h"
 
 UCLASS(CollapseCategories)
-class AKAUDIO_API UInterpTrackAkAudioEvent : public UObject {
+class AKAUDIO_API UInterpTrackAkAudioEvent : public UInterpTrackVectorBase {
     GENERATED_BODY()
 public:
     UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
@@ -13,7 +14,10 @@ public:
     
     UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     uint8 bContinueEventOnMatineeEnd: 1;
+
+    virtual const FString GetEdHelperClassName() const override;
+    virtual const FString GetSlateHelperClassName() const override;
+    virtual UTexture2D* GetTrackIcon() const override;
     
     UInterpTrackAkAudioEvent();
 };
-
