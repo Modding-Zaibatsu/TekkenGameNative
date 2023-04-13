77040443f4d375df7664284374bbfd9e5455d867
Dennis Stanistan <dennisstanistan@gmail.com>
2023-04-13 16:05:22 +0300

    Add CI + CCI factories

---
+++ Source/TekkenGame/Private/CustomizeCharacterItemFactory.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TekkenGameModule.h"
+#include "CustomizeCharacterItemFactory.h"
+
+#include "CustomizeCharacterItem.h"
+
+UCustomizeCharacterItemFactory::UCustomizeCharacterItemFactory() {
+	bCreateNew = true;
+	bEditAfterNew = true;
+	SupportedClass = UCustomizeCharacterItem::StaticClass();
+}
+
+uint32 UCustomizeCharacterItemFactory::GetMenuCategories() const
+{
+	return 1 << 0 | 1 << 6;
+}
+
+UObject* UCustomizeCharacterItemFactory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
+{
+	UCustomizeCharacterItem* CustomizeCharacterItemAsset = NewObject<UCustomizeCharacterItem>(InParent, Class, Name, Flags);
+	return CustomizeCharacterItemAsset;
+}
