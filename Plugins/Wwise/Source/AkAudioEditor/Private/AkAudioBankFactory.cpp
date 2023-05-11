3ada17ecae3f8e55c5decaadf83fed2d20b2c104
Dennis Stanistan <dennisstanistan@gmail.com>
2023-05-11 15:41:48 +0300

    Fix cmake compile error on AkAudioBankFactory.cpp
    changes AkAudioBankFactory to AKAudioBankFactory, otherwise cmake will fail to compile the plugin. silly case sensitivity

--- Plugins/Wwise/Source/AkAudioEditor/Private/AkAudioBankFactory.cpp
+++ Plugins/Wwise/Source/AkAudioEditor/Private/AkAudioBankFactory.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "AkAudioEditorModule.h"
+#include "AKAudioEditorModule.h"
 #include "AkAudioBankFactory.h"
 
 #include "AkAudioBank.h"
